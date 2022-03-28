6c2670fa69092c90ff425d3fa115d785c72ff10f
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2022-03-28 17:06:21 +0200

    styling der produkt-card elemente angepasst

--- src/components/Layout/CategoryBar.js
+++ src/components/Layout/CategoryBar.js
@@ -9,16 +9,49 @@ const CategoryBar = () => {
     const sortedProductCategoryList = sortedProductCategorys.map(category => {
         return (
             <li key={category}>
-                <h1>{category}</h1>
+                <p>{category}</p>
             </li>
         );
     });
 
     return (
         <aside className={css.categorybar}>
+            <h3>Categorys</h3>
             <ul>
                 {sortedProductCategoryList}
             </ul>
+            <hr/>
+            <h3>Rating</h3>
+            <button>
+                <span>4 & Up</span>
+            </button>
+            <a>
+                <p>3 & Up</p>
+            </a>
+            <a>
+                <p>2 & Up</p>
+            </a>
+            <a>
+                <p>1 & Up</p>
+            </a>
+            <hr/>
+            <h3>Price</h3>
+            <a>
+                <p>0 - 20 EUR</p>
+            </a>
+            <a>
+                <p>20 - 50 EUR</p>
+            </a>
+            <a>
+                <p>50 - 100 EUR</p>
+            </a>
+            <a>
+                <p>100 - 200 EUR</p>
+            </a>
+            <a>
+                <p>200 EUR & more</p>
+            </a>
+            <hr/>
         </aside>
     );
 };
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,9 @@
 import {useSelector} from "react-redux";
+import Card from "../UI/Card";
+import star from '../../assets/star-filled.svg'
 
 import css from './Products.module.css'
-import Card from "../UI/Card";
+import card_css from '../UI/Card.module.css'
 
 const Products = () => {
     const apiData = useSelector(state => state.fetch.data)
@@ -9,13 +11,17 @@ const Products = () => {
     const products = apiData.map(product => {
         return (
             <Card key={product.id}>
-                <h1>{product.title}</h1>
-                <img src={product.image} alt={product.title}/>
-                <h2>{product.price} €</h2>
-                <h3>
-                    <span>{product.rating.rate} / </span>
-                    <span>{product.rating.count}</span>
-                </h3>
+                <img className={card_css.image} src={product.image} alt={product.title}/>
+                <h3 className={card_css.title}>{product.title}</h3>
+                <div className={card_css.info}>
+                    <div>
+                        <img className={card_css.star} src={star} alt={'ratingStar'}/>
+                        <span className={card_css.rate}>{product.rating.rate}</span>
+                        <span className={card_css.count}> ({product.rating.count})</span>
+                    </div>
+                    <span className={card_css.price}>{(product.price).toFixed(2)} €</span>
+                </div>
+
             </Card>
         )
     })
